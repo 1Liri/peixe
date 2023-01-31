7be1786767fcb1b4b8813d0a24e9aacd99e46e77
1Liri <guiribeirommii@gmail.com>
2023-01-30 23:16:00 -0300

    versão com a tipologia dos parques

--- peixe.py
+++ peixe.py
@@ -1,3 +1,6 @@
-print("olá")
-print("seu")
-print("mixuruca")
+import pandas as pd 
+df = pd.read_excel("inventario.xlsx")
+print(df.head())
+a = "Parque Estadual"
+filtrado = df.query(f"Tipologia == '{a}'") 
+print(filtrado.head(10))
